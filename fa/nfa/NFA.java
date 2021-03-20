a416eb1ce1964aade2b13d9c134a16932acba3d6
AJ Trantham <ajtrantham@u.boisestate.edu>
2021-03-20 16:57:22 -0600

    initial setup

---
+++ fa/nfa/NFA.java
@@ -0,0 +1,80 @@
+package fa.nfa;
+
+import fa.State;
+import fa.dfa.DFA;
+
+import java.util.Set;
+
+public class NFA implements NFAInterface {
+
+    //set of states should be modeled by a class implementing the java.util.Set interface
+    @Override
+    public DFA getDFA() {
+
+        // this will be hard supposedly
+
+        // Requires traversing NFA. To do this you must implement the breadth-first search (BFS)
+        //algorithm ( a loop iterating over a queue; an element of a queue is a set of NFA states).
+
+        return null;
+    }
+
+    @Override
+    public Set<NFAState> getToState(NFAState from, char onSymb) {
+        return null;
+    }
+
+    @Override
+    public Set<NFAState> eClosure(NFAState s) {
+
+        // TODO: Do this before getDFA()
+//        computes the set of NFA states that can be reached from the argument state s by going
+//        only along ε transitions, including s itself. You must implement it using the depth-first
+//        search algorithm (DFS) using a recursion, i.e., eClosure should invoke itself or another
+//        helper method, e.g., private Set<NFAState> eClosure(NFAState s, Set<NFASate> visited)
+//        that invokes itself
+
+
+        return null;
+    }
+
+    @Override
+    public void addStartState(String name) {
+
+    }
+
+    @Override
+    public void addState(String name) {
+
+    }
+
+    @Override
+    public void addFinalState(String name) {
+
+    }
+
+    @Override
+    public void addTransition(String fromState, char onSymb, String toState) {
+
+    }
+
+    @Override
+    public Set<? extends State> getStates() {
+        return null;
+    }
+
+    @Override
+    public Set<? extends State> getFinalStates() {
+        return null;
+    }
+
+    @Override
+    public State getStartState() {
+        return null;
+    }
+
+    @Override
+    public Set<Character> getABC() {
+        return null;
+    }
+}
